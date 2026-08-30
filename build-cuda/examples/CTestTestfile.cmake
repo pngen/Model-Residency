dd4bc98cc7724e849dfa9f79d44e8dad45f35dd7
pngen <pngen93@gmail.com>
2026-08-30 13:24:56 -0700

    Stop tracking build-cuda artifacts; ignore all build directories

--- build-cuda/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/The Journey/Coding/GitHub/production/Model-Residency/examples
-# Build directory: E:/The Journey/Coding/GitHub/production/Model-Residency/build-cuda/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
